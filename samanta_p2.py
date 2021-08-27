fbb1ad1ab82c5235303a18da4968734bb97bda22
samantagupta99 <87129585+samantagupta99@users.noreply.github.com>
2021-08-27 12:30:26 +0530

    Update samanta_p2.py

--- samanta_p2.py
+++ samanta_p2.py
@@ -10,7 +10,7 @@ spark.sparkContext.setLogLevel("ERROR")
 
 #1
 flights1=spark.read.option("header",True).csv("Project2/flights.csv")
-print("Query 16 : Joining the tables with IATA code  -> flights with airports")
+print("Query 1 : Read in the airports data and Show the top 10.")
 #flights1.printSchema()
 #Printing the Flight dataset(First 10 Row)
 #flights1.show(truncate=False,n=10)
@@ -18,7 +18,7 @@ print("Query 16 : Joining the tables with IATA code  -> flights with airports")
 
 
 #2
-print("Query 16 : Joining the tables with IATA code  -> flights with airports")
+print("Query 2 : Read in the flights data and Show the top 10.")
 #airport1=spark.read.option("header",True).csv("Project2/airports.csv")
 #airport1.printSchema()
 #Printing the Airport dataset(First 10 Row)
@@ -26,7 +26,7 @@ print("Query 16 : Joining the tables with IATA code  -> flights with airports")
 
 
 #3
-print("Query 16 : Joining the tables with IATA code  -> flights with airports")
+print("Query 3 : Read in the airlines data and Show the top 10.")
 #airlines1=spark.read.option("header",True).csv("Project2/airlines.csv")
 #airlines1.printSchema()
 #Printing the Airlines dataset(First 10 Row)
@@ -34,13 +34,13 @@ print("Query 16 : Joining the tables with IATA code  -> flights with airports")
 
 
 #4
-print("Query 16 : Joining the tables with IATA code  -> flights with airports")
+print("Query 4 : Count the number of row and columns of flights dataset.")
 #print((flights1.count(), len(flights1.columns)))
 #flights1.show(5)
 
 
 #5
-print("Query 16 : Joining the tables with IATA code  -> flights with airports")
+print("Query 5 : Select the columns AIRLINE, FLIGHT_NUMBER, TAIL_NUMBER, ORIGIN_AIRPORT, DESTINATION_AIRPORT, SCHEDULED_DEPARTURE FROM flights.")
 #flights1.createOrReplaceTempView('flights')
 #query = "SELECT AIRLINE, FLIGHT_NUMBER, TAIL_NUMBER, ORIGIN_AIRPORT, DESTINATION_AIRPORT, SCHEDULED_DEPARTURE FROM flights LIMIT 10"
 #flights2 = spark.sql(query)
